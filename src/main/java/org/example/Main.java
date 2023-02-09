6f7aff1fc892ef289bbb665cf4d0663028ff9044
maryam_mrb <maryam.mrb94@gmail.com>
2023-02-09 16:36:40 -0500

    add method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("New changes for Testing Travis!");
+       int var = method(4);
+    }
+
+    public static int method(int a){
+        return a;
     }
 }
